2943384aa0f9c4f3686f17bd6cb16dc32f1f94bf
Windel Emman Mascariñas <wemascarinas@gmail.com>
2021-07-22 19:12:04 -0700

    adjust testing

--- rn/src/OCRaProcessor/fallbackBehavior/__tests__/interpretUnexpectedNumberSamples.test.js
+++ rn/src/OCRaProcessor/fallbackBehavior/__tests__/interpretUnexpectedNumberSamples.test.js
@@ -37,28 +37,32 @@ const filenames = ["dev03635419", "dev43876262", "dev81854288"];
 const testData = filenames.map(filenameToTestData);
 const scribbledData = filenameToTestData("scribbled");
 
-function testCoordinates(x, y, key, redPixelData, dataName, expected) {
+function testCoordinates(x, y, key, redPixelData, name, expected) {
   const samples = getNumberSamples(x, y, redPixelData);
-  const testDescription = `${key}: ${dataName}`;
+  const testDescription = `${key}: ${name}`;
   expect([
     testDescription,
     interpretUnexpectedNumberSamples(samples, key),
   ]).toStrictEqual([testDescription, expected]);
 }
 
-function testStaticValues(expectedValues, coordinateSet) {
-  expectedValues.forEach((result, idx) => {
+function testValues(expectedValues, coordinateSet, testDataSet) {
+  expectedValues.forEach((expected, idx) => {
     const [x, y, key] = coordinateSet[idx];
-    testData.forEach(({ name, redPixelData }) =>
-      testCoordinates(x, y, key, redPixelData, name, result)
+    testDataSet.forEach(({ name, redPixelData }) =>
+      testCoordinates(x, y, key, redPixelData, name, expected)
     );
   });
 }
 
-function testVaryingValues(indices, coordinateSet) {
+function testStaticStandardValues(expectedValues, coordinateSet) {
+  testValues(expectedValues, coordinateSet, testData);
+}
+
+function testVaryingValues(indices, coordinateSet, testDataSet) {
   indices.forEach((idx) => {
     const [x, y, key] = coordinateSet[idx];
-    testData.forEach(({ name, redPixelData, panString }) =>
+    testDataSet.forEach(({ name, redPixelData, panString }) =>
       testCoordinates(
         x,
         y,
@@ -69,6 +73,10 @@ function testVaryingValues(indices, coordinateSet) {
       )
     );
   });
+}
+
+function testVaryingStandardValues(indices, coordinateSet) {
+  testVaryingValues(indices, coordinateSet, testData);
 }
 
 function testUnintelligibleStaticValues(expectedValues, coordinateSet) {
@@ -89,72 +97,127 @@ function testUnintelligibleVaryingValues(indices, coordinateSet) {
   });
 }
 
-const originalConsoleError = console.error;
+const originalConsoleLog = console.log;
 
 describe("OCRaProcessor: interpretUnexpectedNumberSamples", () => {
-  // This method is not intended to be called often, so it will throw whenever
-  // it's called. We'll silence it for testing.
-  beforeEach(() => {
-    console.error = jest.fn();
-  });
-  afterEach(() => {
-    console.error = originalConsoleError;
-  });
-
-  describe("Expiry Month", () => {
-    const expectedResults = [0, 5];
-    const coordinates = MONTH_COORDINATES;
-    it("recognizes dev month as 05", () => {
-      testStaticValues(expectedResults, coordinates);
+  describe("Standard Test Cases", () => {
+    // This method is not intended to be called often, so it will log whenever
+    // it's called. We'll silence it for testing.
+    beforeEach(() => {
+      console.log = jest.fn();
+    });
+    afterEach(() => {
+      console.log = originalConsoleLog;
     });
 
-    it("throws when image is unintelligible", () => {
-      testUnintelligibleStaticValues(expectedResults, coordinates);
-    });
-  });
-
-  describe("Expiry Year", () => {
-    const expectedResults = [2, 4];
-    const coordinates = YEAR_COORDINATES;
-    it("recognizes dev year as 24", () => {
-      testStaticValues(expectedResults, coordinates);
-    });
-
-    it("throws when image is unintelligible", () => {
-      testUnintelligibleStaticValues(expectedResults, coordinates);
-    });
-  });
-
-  describe("CVV", () => {
-    const expectedResults = [1, 2, 3];
-    const coordinates = CVV_COORDINATES;
-    it("recognizes dev cvv as 123", () => {
-      testStaticValues(expectedResults, coordinates);
-    });
-
-    it("throws when image is unintelligible", () => {
-      testUnintelligibleStaticValues(expectedResults, coordinates);
-    });
-  });
-
-  describe("PAN", () => {
-    describe("First 8 digits in dev", () => {
-      const expectedResults = [5, 2, 7, 1, 9, 7, 0, 0];
-      const coordinates = PAN_COORDINATES;
-      it("recognizes first 8 PAN in dev as 52719700", () => {
-        testStaticValues(expectedResults, coordinates);
+    describe("Expiry Month", () => {
+      const expectedResults = [0, 5];
+      const coordinates = MONTH_COORDINATES;
+      it("recognizes dev month as 05", () => {
+        testStaticStandardValues(expectedResults, coordinates);
       });
-    });
 
-    describe("Last 8 digits in dev", () => {
-      const indices = [8, 9, 10, 11, 12, 13, 14, 15];
-      const coordinates = PAN_COORDINATES;
-      it("recognizes the varying digits of the last 8 PAN in dev", () => {
-        testVaryingValues(indices, coordinates);
-      });
       it("throws when image is unintelligible", () => {
-        testUnintelligibleVaryingValues(indices, coordinates);
+        testUnintelligibleStaticValues(expectedResults, coordinates);
+      });
+    });
+
+    describe("Expiry Year", () => {
+      const expectedResults = [2, 4];
+      const coordinates = YEAR_COORDINATES;
+      it("recognizes dev year as 24", () => {
+        testStaticStandardValues(expectedResults, coordinates);
+      });
+
+      it("throws when image is unintelligible", () => {
+        testUnintelligibleStaticValues(expectedResults, coordinates);
+      });
+    });
+
+    describe("CVV", () => {
+      const expectedResults = [1, 2, 3];
+      const coordinates = CVV_COORDINATES;
+      it("recognizes dev cvv as 123", () => {
+        testStaticStandardValues(expectedResults, coordinates);
+      });
+
+      it("throws when image is unintelligible", () => {
+        testUnintelligibleStaticValues(expectedResults, coordinates);
+      });
+    });
+
+    describe("PAN", () => {
+      describe("First 8 digits in dev", () => {
+        const expectedResults = [5, 2, 7, 1, 9, 7, 0, 0];
+        const coordinates = PAN_COORDINATES;
+        it("recognizes first 8 PAN in dev as 52719700", () => {
+          testStaticStandardValues(expectedResults, coordinates);
+        });
+      });
+
+      describe("Last 8 digits in dev", () => {
+        const indices = [8, 9, 10, 11, 12, 13, 14, 15];
+        const coordinates = PAN_COORDINATES;
+        it("recognizes the varying digits of the last 8 PAN in dev", () => {
+          testVaryingStandardValues(indices, coordinates);
+        });
+        it("throws when image is unintelligible", () => {
+          testUnintelligibleVaryingValues(indices, coordinates);
+        });
       });
     });
   });
+
+  // It's arguably simplest to test inside of a node/jest environment since our
+  // react native app doesn't have great file system support.
+  // If a card with novel digits is found, save the image to the testing assets directory
+  // with a filename that includes the last 8 digits of the PAN.
+  // Update the novelFilename variable appropriately as well as
+  // the 0's in the expected results to accurate values.
+
+  // describe("Novel Case", () => {
+  //   const novelFilename = "novelXXXXXXXXX";
+  //   const novelTestData = [filenameToTestData(novelFilename)];
+  //   describe("Expiry Month", () => {
+  //     const expectedResults = [0, 0];
+  //     const coordinates = MONTH_COORDINATES;
+  //     it("recognizes novel month", () => {
+  //       testValues(expectedResults, coordinates, novelTestData);
+  //     });
+  //   });
+
+  //   describe("Expiry Year", () => {
+  //     const expectedResults = [0, 0];
+  //     const coordinates = YEAR_COORDINATES;
+  //     it("recognizes novel year", () => {
+  //       testValues(expectedResults, coordinates, novelTestData);
+  //     });
+  //   });
+
+  //   describe("CVV", () => {
+  //     const expectedResults = [0, 0, 0];
+  //     const coordinates = CVV_COORDINATES;
+  //     it("recognizes novel cvv", () => {
+  //       testValues(expectedResults, coordinates, novelTestData);
+  //     });
+  //   });
+
+  //   describe("PAN", () => {
+  //     describe("First 8 digits", () => {
+  //       const expectedResults = [5, 2, 7, 1, 9, 7, 0, 0];
+  //       const coordinates = PAN_COORDINATES;
+  //       it("recognizes first 8 of pan for novel card", () => {
+  //         testValues(expectedResults, coordinates, novelTestData);
+  //       });
+  //     });
+
+  //     describe("Last 8 digits in dev", () => {
+  //       const indices = [8, 9, 10, 11, 12, 13, 14, 15];
+  //       const coordinates = PAN_COORDINATES;
+  //       it("recognizes the varying digits of the last 8 PAN in dev", () => {
+  //         testVaryingValues(indices, coordinates, novelTestData);
+  //       });
+  //     });
+  //   });
+  // });
 });
